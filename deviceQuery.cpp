c29fbec4f18e29a2db0e0242e1f89850b4ce1b31
arjun <arjun@arjun-ThinkCentre-M58p.(none)>
2013-08-06 12:15:09 +0200

    added files to test OpenCL

---
+++ deviceQuery.cpp
@@ -0,0 +1,41 @@
+/*
+ * deviceQuery.cpp
+ *
+ *  Created on: Aug 5, 2013
+ *      Author: arjun
+ */
+
+#include <iostream>
+using namespace std;
+
+//OpenCL includes
+#include <CL/cl.h>
+
+
+int main(int argc, char **argv)
+{
+
+	//cout<<"Hello World!";
+	//int x;
+	//cin>>x;
+
+	//the real stuff starts here
+
+	//platform details
+	cl_uint devicecount=NULL;
+	cl_uint NumPlatforms;
+	clGetPlatformIDs (0, NULL, &NumPlatforms);
+
+	cl_platform_id* PlatformIDs;
+	PlatformIDs = new cl_platform_id[NumPlatforms];
+	clGetPlatformIDs(NumPlatforms, PlatformIDs, NULL);
+
+	cl_device_id cdDevice;
+	clGetDeviceIDs(NULL, CL_DEVICE_TYPE_CPU, 1, &cdDevice, &devicecount);
+
+	cout << NumPlatforms << endl;
+	cout << PlatformIDs << endl;
+	cout << cdDevice << endl;
+	cout << devicecount << endl;
+	return 0;
+}
